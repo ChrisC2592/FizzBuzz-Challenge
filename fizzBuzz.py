c667dc3e25cfd2242103277e2796473eeb28fdd4
ChrisC2592 <ChrisC2592@users.noreply.github.com>
2018-01-30 20:24:49 -0500

    revised

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -6,18 +6,17 @@ def fizzBuzz():
     counter = 1
 
     while(counter <= 100):
-        if ((counter / 3).is_integer() or (counter / 3) == 1.0) and ((counter / 5).is_integer() or (counter / 5) == 1.0):
+        if ((counter % 3) == 0) and ((counter % 5) == 0):
             print("FizzBuzz")
         else:
-            if((counter / 3).is_integer() or (counter / 3) == 1.0):
+            if ((counter % 3) == 0):
                 print("Fizz")
             else:
-                if((counter / 5).is_integer() or (counter/5) == 1.0):
-                    print("Fizz")
+                if ((counter % 5) == 0):
+                    print("Buzz")
                 else:
                     print(counter)
         counter = counter + 1
     return
 
-fizzBuzz()    
-            
+fizzBuzz()
